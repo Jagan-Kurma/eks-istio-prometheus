5962b52d1301990dca25f1168e834b6386f2f7fd
Jagan-Kurma <krmjagan@gmail.com>
2025-12-24 16:05:48 -0600

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "eks_assume" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_role" {
+  name               = "${var.cluster_name}-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
